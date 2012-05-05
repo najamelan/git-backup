f9cea86ae34d40081976278c381e0156dfb4f172
Naja Melan <najamelan@autistici.org>
2012-05-05 04:21:26 +0200

    Add an install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+cp --remove-destination --force git-backup  /usr/lib/git-core/
+cp --remove-destination --force git-restore /usr/lib/git-core/
+
+gzip --to-stdout manpages/git-backup.1  >> /usr/share/man/man1/git-backup.1.gz
+gzip --to-stdout manpages/git-restore.1 >> /usr/share/man/man1/git-restore.1.gz
