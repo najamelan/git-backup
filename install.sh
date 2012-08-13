4cb5f10aff6ce01bff7414a3fc516adc93f9f0a5
Naja Melan <najamelan@autistici.org>
2012-08-13 23:52:25 +0200

    install: correct error of appending man files instead of replacing them

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 cp --remove-destination --force git-backup  /usr/lib/git-core/
 cp --remove-destination --force git-restore /usr/lib/git-core/
 
-gzip --to-stdout manpages/git-backup.1  >> /usr/share/man/man1/git-backup.1.gz
-gzip --to-stdout manpages/git-restore.1 >> /usr/share/man/man1/git-restore.1.gz
+gzip --to-stdout manpages/git-backup.1  > /usr/share/man/man1/git-backup.1.gz
+gzip --to-stdout manpages/git-restore.1 > /usr/share/man/man1/git-restore.1.gz
